821463b3539e9aca1cd415ff74008664d0bc3742
Mohamed ABDENNEBI <mohamed.abdennebi@exakis-nelite.com>
2021-08-30 23:55:21 +0200

    terraform initiation with backend and resource groupe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 0901terraform
+
+## Etape1
+-création du main avec backend(storage du tfstate), required_provider{azurerm},provider (azurerm)
+-test avec un RG
+-command : terraform init
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name = "RG-MOHAMED-ABCD"
+        storage_account_name = "storageaccountmohamed"
+        container_name = "tftf"
+        key = "terraform.state"
+
+    }
+required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = ">= 1.2.0"
+    }
+  }
+}
+
+
+  # Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "RG-ABCD"
+  location = "West Europe"
+}
